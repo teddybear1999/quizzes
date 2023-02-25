26d702f53ba69c06d902cb18d113899e485389dd
Artur <artfpl@gmail.com>
2023-02-25 12:03:34 +0100

    Initializing project

---
+++ src/test/java/pl/afranaso/quizzes/QuizzesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.afranaso.quizzes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
