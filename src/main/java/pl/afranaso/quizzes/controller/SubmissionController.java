5d6d75637634ea2dee1323a7bddd59bf7cba4af3
Artur <artfpl@gmail.com>
2023-08-15 16:30:38 +0200

    Submit of quiz creation bug fix

--- src/main/java/pl/afranaso/quizzes/controller/SubmissionController.java
+++ src/main/java/pl/afranaso/quizzes/controller/SubmissionController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import pl.afranaso.quizzes.dto.SingleQuizDto;
 import pl.afranaso.quizzes.model.Submission;
@@ -12,26 +14,32 @@ import pl.afranaso.quizzes.service.SubmissionService;
 import javax.validation.Valid;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/submissions")
+@Controller
 @RequiredArgsConstructor
 public class SubmissionController {
 
     private final SubmissionService submissionService;
 
-    @GetMapping("/{id}")
+    @GetMapping("submission/{id}")
+    @ResponseBody
     public ResponseEntity<Submission> getSingleSubmission(@PathVariable long id) {
         Optional<Submission> submission = submissionService.getSubmission(id);
         return submission.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("quizz/{id}")
+    @GetMapping("/quizz/{id}")
+    @ResponseBody
     public Page<Submission> getQuizSubmissions(@PathVariable Long id, Pageable pageable) {
         return submissionService.getQuizSubmissions(id, pageable);
     }
 
-    @PostMapping
-    public Submission addSubmission(@RequestBody @Valid SingleQuizDto singleQuizDto) {
-        return submissionService.createSubmission(singleQuizDto);
+    @PostMapping("/submission/create")
+    public String addSubmission(@ModelAttribute @Valid SingleQuizDto singleQuizDto, BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println(result.getAllErrors());
+        }
+        Submission submission = submissionService.createSubmission(singleQuizDto);
+        long id = submission.getId();
+        return "redirect:/submission/" + id;
     }
 }
